0eda33a3c28ad60b16146415e8bb74c99e0767a1
Daniel <potrayarrick@gmail.com>
2015-08-06 14:27:36 +0200

    Added a simple, yet functional, drawer. And a few fragments to work with it. Closes #14.

--- app/src/main/java/es/potrayarrick/pacts/MainActivity.java
+++ app/src/main/java/es/potrayarrick/pacts/MainActivity.java
@@ -1,20 +1,21 @@
 package es.potrayarrick.pacts;
 
-import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements
+        NavigationDrawerFragment.NavigationDrawerCallbacks,
+        ProfileFragment.OnFragmentInteractionListener,
+        FriendsFragment.OnFragmentInteractionListener,
+        PactsFragment.OnFragmentInteractionListener {
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -26,14 +27,32 @@ public class MainActivity extends AppCompatActivity
      */
     private CharSequence mTitle;
 
+    private ProfileFragment mProfileFragment;
+    private PactsFragment mPactsFragment;
+    private FriendsFragment mFriendsFragment;
+
+    private ArrayList<android.app.Fragment> fragments;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        //Create fragments
+        mProfileFragment = new ProfileFragment();
+        mPactsFragment = new PactsFragment();
+        mFriendsFragment = new FriendsFragment();
+
+        fragments = new ArrayList<>();
+        fragments.add(mPactsFragment);
+        fragments.add(mProfileFragment);
+        fragments.add(mFriendsFragment);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)  getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
+
+        //Load the pacts fragment
+        getFragmentManager().beginTransaction().replace(R.id.fragment_container, fragments.get(0)).commit();
 
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
@@ -43,11 +62,11 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
+        // Update the main content by replacing fragments
+        Log.d("main", "onNavigationDrawerItemSelected " + position);
+        if (fragments != null) {
+            getFragmentManager().beginTransaction().replace(R.id.fragment_container, fragments.get(position)).commit();
+        }
     }
 
     public void onSectionAttached(int number) {
@@ -100,44 +119,8 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
     }
-
 }
